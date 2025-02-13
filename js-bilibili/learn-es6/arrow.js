c8a2b37699f4e3cb15cd47c1ec7f7bdd2c608ee7
wang xi <1303780098@qq.com>
2025-02-13 23:57:57 +0800

    add arrow function file

---
+++ js-bilibili/learn-es6/arrow.js
@@ -0,0 +1,12 @@
+class C {
+  a = 1;
+  autoBoundMethod = () => {
+    console.log(this.a);
+  };
+}
+
+const c = new C();
+c.autoBoundMethod(); // 1
+const { autoBoundMethod } = c;
+autoBoundMethod(); // 1
+// 如果这是普通方法，此时应该是 undefined
